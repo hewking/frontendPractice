8302ecc2198b5fd995096f4085f83e2f38446d10
hewking <604910070@qq.com>
2019-03-11 14:42:26 +0800

    es6 module

--- es6/module/circle.js
+++ es6/module/circle.js
@@ -1,3 +1,15 @@
 export function area(radius) {
 	return Math.PI * radius * radius
 }
+
+export default function(){
+	console.log('匿名输出')
+}
+
+// export default function width(){
+// 	console.log("输出宽度")
+// }
+
+function ll(){}
+
+export ll
