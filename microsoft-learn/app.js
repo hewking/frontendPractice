c6df6241306a4933f56dcbafbdb25d99780226fc
jianhao <chengjianhao@fcbox.com>
2021-08-21 14:55:48 +0800

    feature: update

--- microsoft-learn/app.js
+++ microsoft-learn/app.js
@@ -3,12 +3,14 @@
 
 const switcher = document.querySelector('.btn');
 
-switcher.addEventListener('click', function () {
+function toogleTheme() {
     // 切换类选择器
     document.body.classList.toggle('dark-theme')
 
     var className = document.body.className;
     if (className == "light-theme") {
+        // this 表示调用这个函数的对象, addEventListener 中， 函数this 指向
+        // 出发事件的元素
         this.textContent = "Dark";
     } else {
         this.textContent = "Light";
@@ -16,4 +18,6 @@ switcher.addEventListener('click', function () {
 
     console.log('current class name: ' + className);
 
-});
+}
+
+switcher.addEventListener('click', toogleTheme);
