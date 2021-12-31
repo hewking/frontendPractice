88631f17b5352b30e219d42c67c7f30a53c90485
jianhao <chengjianhao@fcbox.com>
2021-12-31 17:37:36 +0800

    feature: add router-tutorial

--- web-dev-beginner/banking-project-react/src/App.js
+++ web-dev-beginner/banking-project-react/src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './components/login';
-import DashBoard from './components/dashboard';
-import {
-  BrowserRouter,
-  Routes,
-  Route, 
-} from "react-router-dom";
-import {browserHistory} from "react-router";
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/login";
+import DashBoard from "./components/dashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { browserHistory } from "react-router";
 
 function App() {
-  return <BrowserRouter history={browserHistory }>
-  <Routes path="/">
-    <Route index elements={<Login/>}/>
-    <Route path="dashboard" elements={<DashBoard/>}/>
-  </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter history={browserHistory}>
+      <Routes path="/">
+        <Route index elements={<Login />} />
+        <Route path="dashboard" elements={<DashBoard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
