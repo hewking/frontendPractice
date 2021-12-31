1b11f29f7b879f159183b69baf7847cbf698e187
jianhao <chengjianhao@fcbox.com>
2021-12-31 18:10:09 +0800

    feature: add router basic sample

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Router!</h1>
+      <nav style={{
+        borderbottom: '1px solid',
+        paddingbottom: '1rem'
+      }}>
+      <Link to="/invoices">Invoices</Link> | {""}
+      <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   );
 }
--- router-tutorial/src/index.js
+++ router-tutorial/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
