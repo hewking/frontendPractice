33ea23b7ea8a33427488523098f23329cd436d4b
jianhao <chengjianhao@fcbox.com>
2022-01-05 14:33:26 +0800

    feature: searchParams, custom behavior

--- router-tutorial/src/routes/invoices.jsx
+++ router-tutorial/src/routes/invoices.jsx
@@ -1,27 +1,46 @@
 import { getInvoices } from "../data.js";
-import { Link, Outlet, NavLink } from "react-router-dom";
+import { Link, Outlet, NavLink, useSearchParams } from "react-router-dom";
+import QueryNavLink from "../components/queryNavLink.jsx";
 
 export default function Invoices() {
   let invoices = getInvoices();
+  let [searchParams, setSearchParams] = useSearchParams();
   return (
     <div style={{ display: "flex" }}>
       <nav style={{ borderright: "solid 1px", padding: "1rem" }}>
-        {invoices.map((invoice) => {
+        <input value={searchParams.get("filter") || ""}
+        onChange={(event) => {
+          let filter = event.target.value
+          if (filter) {
+            setSearchParams({ filter });
+          } else { 
+            setSearchParams({ });
+          }
+        }}>
+        </input>
+        {invoices
+        .filter(invoice => {
+          if (!searchParams.get("filter")) {
+            return true;
+          }
+          return invoice.name.toLowerCase().includes(searchParams.get("filter").toLowerCase());
+        })
+        .map((invoice) => {
           return (
-            <NavLink
+            <QueryNavLink
               style={({ isActive }) => {
                 return {
                   display: "block",
                   margin: "1rem 0",
                   color: isActive ? "red" : "",
-                };
+                }
               }}
               to={`/invoices/${invoice.number}`}
               //   列表需要设置key 属性，key 为组件默认属性
               key={invoices.number}
             >
               {invoice.name}
-            </NavLink>
+            </QueryNavLink>
           );
         })}
       </nav>
