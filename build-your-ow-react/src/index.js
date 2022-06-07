595a17216513848485823db653f13c814e12c816
jianhao <chengjianhao@fcbox.com>
2022-06-07 18:34:53 +0800

    feature:  add Didact 函数式组件和hook

--- build-your-ow-react/src/index.js
+++ build-your-ow-react/src/index.js
@@ -62,20 +62,33 @@ function commitWork(fiber) {
     return;
   }
 
-  let domParent = fiber.parent.dom;
+  let domParentFiber = fiber.parent;
+  while(!domParentFiber.dom) {
+    domParentFiber = domParentFiber.parent;
+  }
+
+  let domParent = domParentFiber.dom;
  
   if (fiber.effectTag === "PLACEMENT" &&
    fiber.dom != null) {
-    domParent.appendChild(fiber.dom && fiber.dom != null);
+    domParent.appendChild(fiber.dom);
   } else if (fiber.effectTag === "UPDATE"){
     updateDOM(fiber.dom, fiber.props, fiber.alternate.props);
   } else {
-    domParent.removeChild(fiber.dom);
+    commitDelection(fiber, domParent);
   }
 
   commitWork(fiber.child);
   commitWork(fiber.sibling);
 
+}
+
+function commitDelection(fiber, domParent){
+  if (fiber.dom) {
+    domParent.removeChild(fiber.dom);
+  } else {
+    commitDelection(fiber.child, domParent);
+  }
 }
 
 
@@ -176,12 +189,18 @@ requestIdleCallback(workLoop);
 
  */
 function performUnitOfWork(fiber) {
-  if(!fiber.dom) {
-    fiber.dom = createDom(fiber);
-  }
 
-  const elements = fiber.props.children;
-  reconcileChildren(fiber, elements);
+  /**
+   * 1. 函数组件的 fiber 没有 DOM 节点
+     2. 并且子节点由函数运行得来而不是直接从 props 属性中获取
+   */
+  const isFunctionComponent =
+    fiber.type instanceof Function
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber)
+  } else {
+    updateHostComponent(fiber)
+  }
 
   if (fiber.child) {
     return fiber.child;
@@ -197,6 +216,59 @@ function performUnitOfWork(fiber) {
   }
 
   return nextFiber;
+
+}
+
+let wipFiber = null;
+let hookIndex = null;
+
+function updateFunctionComponent(fiber) {
+  wipFiber = fiber;
+  hookIndex = 0;
+  wipFiber.hook = [];
+
+  const children = fiber.type(fiber.props);
+  reconcileChildren(fiber, children);
+}
+
+function updateHostComponent(fiber){
+  if(!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  const elements = fiber.props.children;
+  reconcileChildren(fiber, elements);
+}
+
+function useState(initial){
+  let oldHook = wipFiber.alternate && wipFiber.alternate.hook &&
+    wipFiber.alternate.hook[hookIndex];
+
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: [],
+  }
+
+  const actions = oldHook ? oldHook.queue : [];
+  actions.forEach(action => {
+    hook.state = action(hook.state);
+  });
+
+  const setState = action => {
+    hook.queue.push(action);
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot,
+    }
+    nextUnitOfWork = wipRoot;
+    deletions = [];
+  }
+
+  wipFiber.hooks.push(hook);
+  hookIndex ++; 
+
+  return [hook.state, setState]
 
 }
 
@@ -257,7 +329,8 @@ function reconcileChildren(wipFiber, elements) {
 
 const Didact = {
   createElement,
-  render
+  render, 
+  userState,
 };
 
 /** @jsx Didact.createElement */
