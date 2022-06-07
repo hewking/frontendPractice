15ec6fd86fdd88ea04b38b47ee9d78507ca7ca8a
jianhao <chengjianhao@fcbox.com>
2022-06-07 17:49:18 +0800

    feature: 支持didact reconcil

--- build-your-ow-react/src/index.js
+++ build-your-ow-react/src/index.js
@@ -42,15 +42,19 @@ function render(element, container) {
     dom: container,
     props: {
       children: [element]
-    }
+    },
+    alternate: currentRoot
   }
-
+  deletions = []; 
   nextUnitOfWork = wipRoot;
   
 }
 
 function commitRoot(){
   commitWork(wipRoot.child);
+  deletions.forEach(commitWork);
+  currentRoot = wipRoot
+  wipRoot = null;
 }
 
 function commitWork(fiber) {
@@ -59,14 +63,84 @@ function commitWork(fiber) {
   }
 
   let domParent = fiber.parent.dom;
-  domParent.appendChild(fiber.dom);
+ 
+  if (fiber.effectTag === "PLACEMENT" &&
+   fiber.dom != null) {
+    domParent.appendChild(fiber.dom && fiber.dom != null);
+  } else if (fiber.effectTag === "UPDATE"){
+    updateDOM(fiber.dom, fiber.props, fiber.alternate.props);
+  } else {
+    domParent.removeChild(fiber.dom);
+  }
+
   commitWork(fiber.child);
   commitWork(fiber.sibling);
 
 }
 
+
+const isEvent = key => key.startsWith("on")
+const isProperty = key =>
+  key !== "children" && !isEvent(key)
+const isNew = (prev, next) => key =>
+  prev[key] !== next[key]
+const isGone = (prev, next) => key => !(key in next)
+function updateDom(dom, prevProps, nextProps) {
+  //Remove old or changed event listeners
+  Object.keys(prevProps)
+  .filter(isEvent)
+  .filter(
+    key =>
+      !(key in nextProps) ||
+      isNew(prevProps, nextProps)(key)
+  )
+  .forEach(name => {
+    const eventType = name
+      .toLowerCase()
+      .substring(2)
+    dom.removeEventListener(
+      eventType,
+      prevProps[name]
+    )
+  })
+
+  // Remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = ""
+    })
+​
+  // Set new or changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = nextProps[name]
+    })
+
+     // Add event listeners
+  Object.keys(nextProps)
+  .filter(isEvent)
+  .filter(isNew(prevProps, nextProps))
+  .forEach(name => {
+    const eventType = name
+      .toLowerCase()
+      .substring(2)
+    dom.addEventListener(
+      eventType,
+      nextProps[name]
+    )
+  })
+
+}
+
 let nextUnitOfWork = null;
 let wipRoot = null;
+let currentRoot = null;
+let deletions = null;
+
 
 function workLoop(deadline) {
   let shouldYield = false;
@@ -107,26 +181,7 @@ function performUnitOfWork(fiber) {
   }
 
   const elements = fiber.props.children;
-
-  let index = 0;
-
-  while(index < elements.length) {
-    const element = elements[index];
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
-    }
-    if(index === 0) {
-      fiber.child = newFiber;
-    } else {
-      prevSibling.sibling = newFiber;
-    }
-
-    prevSibling = newFiber;
-    index++;
-  }
+  reconcileChildren(fiber, elements);
 
   if (fiber.child) {
     return fiber.child;
@@ -146,6 +201,57 @@ function performUnitOfWork(fiber) {
 }
 
 
+function reconcileChildren(wipFiber, elements) {
+  let index = 0;
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
+  let prevSibling = null;
+
+  while(index < elements.length 
+    || oldFiber != null) {
+    const element = elements[index];
+    let newFiber = null;
+    let sameType = oldFiber && element && element.type == oldFiber.type;
+
+    if (sameType) {
+      // 相同类型的节点，更新 props
+     newFiber = {
+        type: oldFiber.type,
+        props: oldFiber.props,
+        parent: wipFiber,
+        dom: oldFiber.dom,
+        alternate: oldFiber,
+        effectTag: "UPDATE"
+      }
+    }
+
+    if (element && !sameType) {
+      // 新的节点，添加到 DOM 上
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        parent: wipFiber,
+        dom: null,
+        alternate: null,
+        effectTag: "PLACEMENT"
+      }
+    }
+
+    if (oldFiber && !sameType) {
+      // 删除旧的节点
+      oldFiber.effectTag = "DELETION";
+      deletions.push(oldFiber);
+    }
+
+    if(index === 0) {
+      wipFiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+}
 
 
 
