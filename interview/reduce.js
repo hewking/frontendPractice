4e0ff003cf3a9f183a2b01fca4488f4f7882966f
jianhao <chengjianhao@fcbox.com>
2022-07-12 19:32:16 +0800

    feature: add reduce

---
+++ interview/reduce.js
@@ -0,0 +1,8 @@
+
+function Array.prototype.reduce(callback, initialValue) {
+  let accumulator = initialValue;
+  for (let i = 0; i < this.length; i++) {
+    accumulator = callback(accumulator, this[i], i, this);
+  }
+  return accumulator;
+}
