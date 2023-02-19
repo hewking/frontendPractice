2b2b632307f5a5f45d97b94ed71384f9d9163570
jianhao <cjhcoder@qq.com>
2023-02-19 20:48:10 +0800

    Increase reliability of recordings and tests
    - Declare a global variable `foo` with a value
    - Assign values to `foo` and `bar`
    - Call `Math.floor` on the assigned values
    
    [ts-2023/src/index.ts]
    - Declare the variable `foo`
    - Add a global scope
    - Assign `foo` and `bar` values
    - Add a call to `Math.floor`

--- ts-2023/src/index.ts
+++ ts-2023/src/index.ts
@@ -2,5 +2,15 @@ import fs from "fs";
 import { hello } from "./helper.js";
 import { yo } from "./helper.cjs";
 
+declare let foo: number;
+
+declare global {
+}
+
+foo = 12;
+bar = 22;
+
 console.log(hello);
 console.log(yo);
+
+Math.floor
