f7c0baf36b9098421ccf81de444edbe3f213294d
jianhao <chengjianhao@yzw.cn>
2023-06-04 23:57:24 +0800

    feat: add webpack-npm 包发布，支持ts

--- webpack-demos/webpack.config.js
+++ webpack-demos/webpack.config.js
@@ -1,18 +1,20 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-    entry: "./src/entry.js",
-    output: {
-        // path 需要绝对路径
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        // webpack 2.0 之后不使用loaders, 使用rules
-        rules : [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader'
-        }]
-    }
+  entry: "./src/entry.js",
+  output: {
+    // path 需要绝对路径
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    // webpack 2.0 之后不使用loaders, 使用rules
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+      },
+    ],
+  },
 };
