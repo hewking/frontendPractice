e3ac09c02658f38c4a484feccd2900ae265ae8ae
jianhao <cjhcoder@qq.com>
2023-11-05 17:59:49 +0800

    feat: update

--- webpack-npm/webpack.config.js
+++ webpack-npm/webpack.config.js
@@ -1,21 +1,24 @@
 module.exports = {
-    entry: "./src/index.ts",
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: "ts-loader",
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-    },
-    output: {
-      filename: "index.js", // 需要跟你在src文件夹中导出文件的文件名一致
-      globalObject: "this",
-      libraryTarget: "umd", //支持库的引入方式
-    },
-  };
-  
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "index.js", // 需要跟你在src文件夹中导出文件的文件名一致
+    globalObject: "this",
+    libraryTarget: "umd", //支持库的引入方式
+    library: "myLib", // 库的名称
+  },
+  devServer: {
+    publicPath: "/dist",
+  },
+};
