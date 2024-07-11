c22749ce60d3f48a918898409f64bc59a2a89287
jianhao <cjhcoder@qq.com>
2024-07-11 23:18:37 +0800

    fix: 配置 splitchunks all

--- webpack-demo-start/webpack.config.js
+++ webpack-demo-start/webpack.config.js
@@ -1,25 +1,20 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
+  entry: "./src/index.js",
+  mode: "development",
   entry: {
-    index: {
-        import: './src/index.js',
-        dependOn: 'shared'
-    },
-    another: {
-        import:'./src/another-module.js',
-        dependOn: 'shared'
-    },
-    shared: 'lodash',
+    index: "./src/index.js",
+    another: "./src/another-module.js",
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   optimization: {
-    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: "all",
+    },
   },
 };
