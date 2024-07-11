277fa67219107321c5f8c4a296edf1c5cd5f5163
jianhao <cjhcoder@qq.com>
2024-07-11 23:09:44 +0800

    feat: add webpack 官方文档demo

---
+++ webpack-demo-start/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    index: {
+        import: './src/index.js',
+        dependOn: 'shared'
+    },
+    another: {
+        import:'./src/another-module.js',
+        dependOn: 'shared'
+    },
+    shared: 'lodash',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
